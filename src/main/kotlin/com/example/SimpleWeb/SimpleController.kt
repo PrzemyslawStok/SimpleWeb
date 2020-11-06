517dcae49d830ec0f5deca4d69104a5982e38884
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-11-06 18:44:45 +0100

    cd..

--- src/main/kotlin/com/example/SimpleWeb/SimpleController.kt
+++ src/main/kotlin/com/example/SimpleWeb/SimpleController.kt
@@ -1,17 +1,21 @@
 package com.example.SimpleWeb
 
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@Controller
 class SimpleController {
     @RequestMapping("")
+    @ResponseBody
     fun getHome():String{
         return "Strona główna"
     }
-    @RequestMapping("info")
+    @RequestMapping("index")
     fun getInfo():String{
-        return "info"
+        return "index"
     }
 
 }
